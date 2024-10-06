6122644c5238802838fed487d55d44245e1584ba
dom <dpsirott@gmail.com>
2024-10-05 22:13:47 -0400

    lexer seems to be working now

--- jsf.c
+++ jsf.c
@@ -3,6 +3,11 @@
 #include <string.h>
 
 #define JSON_WHITESPACE ((const char[]) { ' ', '\t', '\b', '\n', '\r' })
+#define PIPE_SIZE 256
+#define TOKEN_VALUE_SIZE 64
+#define TOKENS_SIZE 128
+#define STRING_SIZE 64
+#define NUMBER_SIZE 8
 
 typedef enum
 {
@@ -22,7 +27,7 @@ typedef enum
 typedef struct token
 {
     token_type_t type;
-    char value[8192];
+    char value[TOKEN_VALUE_SIZE];
 } token_t;
 
 int is_digit(char c)
@@ -44,59 +49,67 @@ int is_whitespace(char c)
 
 int json_lexer(char *pipe, int len)
 {
-    token_t tokens[65536];
+    token_t tokens[TOKENS_SIZE];
     int t = 0;
     for (int i = 0; i < len; i++)
     {
         char c = pipe[i];
+        printf("beginning with index %d and char %c [%d]\n", i, c, c);
         token_t token;
         switch (c)
         {
             case ',':
                 token.type = JSON_COMMA;
                 strcpy(token.value, ",");
+                printf("\tcomma token added\n");
                 break;
             case ':':
                 token.type = JSON_COLON;
                 strcpy(token.value, ":");
+                printf("\tcolon token added\n");
                 break;
             case '[':
                 token.type = JSON_OPENBRACKET;
                 strcpy(token.value, "[");
+                printf("\topen bracket token added\n");
                 break;
             case ']':
                 token.type = JSON_CLOSEBRACKET;
                 strcpy(token.value, "]");
+                printf("\tclose bracket token added\n");
                 break;
             case '{':
                 token.type = JSON_OPENBRACE;
                 strcpy(token.value, "{");
+                printf("\topen brace token added\n");
                 break;
             case '}':
                 token.type = JSON_CLOSEBRACE;
                 strcpy(token.value, "}");
+                printf("\tclose brace token added\n");
                 break;
-            case '"':
+            case '\"':
                 token.type = JSON_STRING;
-                char str[8192];
+                char str[STRING_SIZE];
                 i++;
                 int s = 0;
-                while (i < len && pipe[i] != '"')
+                while (i < len && pipe[i] != '\"')
                 {
                     str[s++] = pipe[i++];
                 }
                 str[s] = '\0';
-                i++;
                 strcpy(token.value, str);
+                printf("\tstring token added: %s\n", str);
                 break;
             default:
                 if (is_whitespace(c))
                 {
-                    i++;
+                    printf("\tfound whitespace\n");
+                    continue;
                 }
                 else if (is_digit(c))
                 {
-                    char num[8192];
+                    char num[NUMBER_SIZE];
                     int n = 0;
                     while (i < len && is_digit(pipe[i]))
                     {
@@ -105,6 +118,7 @@ int json_lexer(char *pipe, int len)
                     num[n] = '\0';
                     token.type = JSON_NUMBER;
                     strcpy(token.value, num);
+                    printf("\tnumber token added: %s\n", num);
                 }
                 else if (c == 't' && (i + 3 < len))
                 {
@@ -114,6 +128,7 @@ int json_lexer(char *pipe, int len)
                         token.type = JSON_BOOLEAN;
                         strcpy(token.value, "true");
                         i += 4;
+                        printf("\tboolean token added: %s\n", val);
                     }
                 }
                 else if (c == 'f' && (i + 4 < len))
@@ -124,6 +139,7 @@ int json_lexer(char *pipe, int len)
                         token.type = JSON_BOOLEAN;
                         strcpy(token.value, "false");
                         i += 5;
+                        printf("\tboolean token added: %s\n", val);
                     }
                 }
                 else if (c == 'n' && (i + 3 < len))
@@ -134,11 +150,12 @@ int json_lexer(char *pipe, int len)
                         token.type = JSON_NULL;
                         strcpy(token.value, "null");
                         i += 4;
+                        printf("\tnull token added: %s\n", val);
                     }
                 }
                 else
                 {
-                    fprintf(stderr, "Unknown or invalid character at index %d. Aborting.\n", i);
+                    fprintf(stderr, "Unknown or invalid character %c [%d] found at index %d. Aborting.\n", c, c, i);
                     return -1;
                 }
                 break;
@@ -158,14 +175,14 @@ int main(int argc, char **argv)
     if (!isatty(fileno(stdin)))
     {
         int i = 0;
-        char pipe[65536];
+        char pipe[PIPE_SIZE];
 
         while ((pipe[i++] = getchar()) != -1);
         pipe[i - 1] = '\0';
 
-        fprintf(stdout, "piped content:\n%s\n", pipe);
+        fprintf(stdout, "piped content:\n%s\nlen: %d\n", pipe, i - 1);
 
-        int result = json_lexer(pipe, i);
+        int result = json_lexer(pipe, i - 1);
         if (result == -1)
         {
             return 1;
@@ -173,7 +190,7 @@ int main(int argc, char **argv)
     }
     else
     {
-        printf("wtf r u doin\n");
+        printf("Only piped/stdin is supported so far.\n");
     }
     return 0;
 }
